98e13abf4aac7cc915f71e3d747e1f58c869c27e
Keaton Law <keaton.c.law@gmail.com>
2025-04-07 20:45:03 -0500

    scope and closure

--- closure.js
+++ closure.js
@@ -8,7 +8,21 @@ function outer() {
   }
   
   const count = outer();
-  count(); // ?
-  count(); // ?
-  count(); // ?
+  count(); // 1
+  count(); // 2
+  count(); // 3
   
+
+// here I'm going to make a function that returns a new function that multiplies by a number that I passed in.
+
+function makeMultiple(multiplier) {
+    return function (number) {
+        return number * multiplier;
+    }
+}
+
+const double = makeMultiple(2);
+console.log(double(5))
+
+//double is defined as the base function I already made and it multiplies by 2 see line 18, where make multiple takes the multiplier. 2 is the multiplier in this case.
+// when I console log double, its going to return the number i pass in, and the multiplier. like it says on line 20.
